6758f4a4a395ddebb69c86e0b4f6e07fe6605f80
Abduljwad-AlFaifi <abduljwad2001@gmail.com>
2025-12-15 14:48:45 +0300

    Delete content of Error.py.ext

--- Error.py
+++ Error.py
@@ -1,10 +1,1 @@
-log_file = open("server_log.txt", "r")
-lines = log_file.readlines()
-error_lines = []
-for line in lines:
-    if "ERROR" in line:
-        error_lines.append(line)
-    error_file = open("errors_only.txt", "w")
-    error_file.writelines(error_lines)
-    error_file.close()
-print("Number of errors found:", len(error_lines))
+
